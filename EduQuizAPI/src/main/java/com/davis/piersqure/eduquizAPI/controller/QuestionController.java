a05a759c2048971ef5b75e181da40eb4e98c9c88
dvsnayak <yello.davis@gmail.com>
2024-03-18 22:33:23 +0530

    logger added in the project

--- EduQuizAPI/src/main/java/com/davis/piersqure/eduquizAPI/controller/QuestionController.java
+++ EduQuizAPI/src/main/java/com/davis/piersqure/eduquizAPI/controller/QuestionController.java
@@ -17,35 +17,48 @@ import com.davis.piersqure.eduquizAPI.dto.QuestionDto;
 import com.davis.piersqure.eduquizAPI.entity.Question;
 import com.davis.piersqure.eduquizAPI.service.QuestionService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/api/questions")
+@Slf4j
 public class QuestionController {
 	
-
+	//Logger logger = LoggerFactory.getLogger(QuestionController.class);
 
     @Autowired
     private QuestionService questionService;
 
     @GetMapping
     public List<Question> getAllQuestions() {
-        return questionService.getAllQuestions();
+    	log.info("Fetching all questions.");
+    	List<Question> questions = questionService.getAllQuestions();
+    	log.info("Retrieved {} questions.", questions.size());
+        return questions;
+      //  return questionService.getAllQuestions();
     }
 
     @PostMapping
     public ResponseEntity<Question> createQuestion(@RequestBody Question question) {
+    	log.info("Creating a new question: {}", question);
         Question createdQuestion = questionService.createQuestion(question);
+        log.info("Question created: {}", createdQuestion);
         return new ResponseEntity<>(createdQuestion, HttpStatus.CREATED);
     }
     
     @DeleteMapping("/{questionId}")
     public ResponseEntity<Void> deleteQuestion(@PathVariable Long questionId) {
+    	log.info("Deleting question with ID: {}", questionId);
         questionService.deleteQuestionWithAnswers(questionId);
+        log.info("Question with ID {} deleted successfully.", questionId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
     
     @GetMapping("/generateQuestionSet")
     public ResponseEntity<List<QuestionDto>> generateQuestionSet() {
+    	log.info("Generating question set.");
         List<QuestionDto> questionSet = questionService.generateQuestionSet();
+        log.info("Question set generated successfully.");
         return new ResponseEntity<>(questionSet, HttpStatus.OK);
     }
 
